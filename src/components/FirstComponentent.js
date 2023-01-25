285d3ca3c83a950c6000749ef7e029124313d4a0
maria <mvbri10@gmail.com>
2023-01-24 21:19:13 -0400

    Aplicamos useState para hacer funcionar evento click en botón

--- src/components/FirstComponentent.js
+++ src/components/FirstComponentent.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const FirstComponent = () => {
-	let name = 'Maria';
 	let web = 'https://mvbri.github.io/';
+	let nameEntry = 'Maria V';
+
+	const [name, setName] = useState(nameEntry);
+
+	let courses = [
+		'Master en React',
+		'Master en JavaScript',
+		'Master en CSS',
+		'Master de PHP',
+	];
+
+	const changeName = (newName) => {
+		setName(newName);
+	};
 
 	return (
 		<div>
 			<h2>First component</h2>
 			<p>Mi nombre es: {name}</p>
 			<p>Mi web es: {web}</p>
+
+			<button onClick={(e) => changeName('Maria Victoria')}>Un botón</button>
+
+			<h3>Curso</h3>
+			<ul>
+				{courses.map((course, index) => {
+					return <li key={index}>{course}</li>;
+				})}
+			</ul>
 		</div>
 	);
 };
