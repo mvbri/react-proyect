d7bf340d596d22fffda2f4579182a9babeb85a59
maria <mvbri10@gmail.com>
2023-01-24 20:21:07 -0400

    Repaso de React - Paso de variables dentro de un componente a un template

---
+++ src/components/FirstComponentent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const FirstComponent = () => {
+	let name = 'Maria';
+	let web = 'https://mvbri.github.io/';
+
+	return (
+		<div>
+			<h2>First component</h2>
+		</div>
+	);
+};
